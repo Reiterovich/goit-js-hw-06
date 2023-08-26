2a4f985dbdb55da14dbadcc4714684f2a3a87e5b
Reiterovich <reiterovich.andriy@gmail.com>
2023-08-26 18:15:31 +0300

    add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulList = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const prodEl = document.createElement("li");
+  prodEl.textContent = element;
+  ulList.append(prodEl);
+});
