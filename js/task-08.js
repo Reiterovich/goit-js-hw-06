ee9aae288ddfcbc8c558764ed12339e810e0a1c8
Reiterovich <reiterovich.andriy@gmail.com>
2023-09-04 16:49:02 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const onLogin = document.querySelector(".login-form");
 
+onLogin.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const user = {
+    email,
+    password,
+  };
+  console.log(user);
+  form.reset();
+}
