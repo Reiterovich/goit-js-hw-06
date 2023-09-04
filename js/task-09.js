b6b972f2efb794ea533388dc38002ece3f171b0c
Reiterovich <reiterovich.andriy@gmail.com>
2023-09-04 17:09:43 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor());
+
+const onBody = document.querySelector("body");
+const onBtn = document.querySelector(".change-color");
+const onSpan = document.querySelector(".color");
+
+onBtn.addEventListener("click", fonBody);
+
+function fonBody(event) {
+  const color = getRandomHexColor();
+  onBody.style.backgroundColor = color;
+  onSpan.textContent = color;
+}
